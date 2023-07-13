aad018a333b421a71572d157c627665e9fae3b16
Ocirled <delricolie111@gmail.com>
2023-07-13 20:50:30 +0700

    Portofolio TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        gochi: ["Gochi Hand"]
+      },
+
+      colors: {
+        green: "#0EF6CC",
+        gray: "#3A4F50",
+        white: "#F4FEFD",
+        dark: "#1B2223",
+        textGray:'#64748b'
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
